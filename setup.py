f11cd6c69981ff8af7b47d8d600d6b10289c9cff
Mitchell <mitchellmccaig@outlook.com>
2021-10-28 13:57:12 -0600

    initialized repo with cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Dashboarding and time series project of Edmonton Housing Prices',
+    author='Mitchell McCaig',
+    license='',
+)
