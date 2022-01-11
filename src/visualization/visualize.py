5f2063286ff692133b38923172b30fa830cdbef6
Mitchell <mitchellmccaig@outlook.com>
2022-01-10 17:21:13 -0700

    created new value_per_ward function for ward map plot

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -4,8 +4,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 def value_per_year(df, measurement, ward='All'):
-    year_list = df['Assessment Year'].unique()
-    year_list = np.sort(year_list)
+    year_list = sorted(df['Assessment Year'].unique())
     stat_list = []
     if ward != 'All':
         df = df[df['Ward'] == ward]
@@ -21,9 +20,22 @@ def value_per_year(df, measurement, ward='All'):
             raise ValueError('Not a correct measurement, use either Average or Median.')
     return stat_list, year_list
 
+def value_per_ward(df, measurement, year='All'):
+    ward_list = sorted(df['Ward'].unique())
+    ward_price_list = []
+    if year != 'All':
+        df = df[df['Assessment Year'] == year]
+    for ward in ward_list:
+        if measurement == 'Average':
+            mask = df[df['Ward'] == ward]
+            ward_price_list.append(mask['Assessed Value'].mean())
+        elif measurement == 'Median':
+            mask = df[df['Ward'] == ward]
+            ward_price_list.append(mask['Assessed Value'].median())
+        else:
+            raise ValueError('Not a correct measurement, use either Average or Median.')
+    return ward_price_list, ward_list    
+
 # resi_house_data = pd.read_csv('prop_price/data/processed/Historical_clean.csv', dtype={'Suite': object})
-# ward_list = sorted(resi_house_data['Ward'].unique())
-# print(ward_list)
-# print(resi_house_data.head())
-# stat_list, year_list = value_per_year(resi_house_data, 'Average', ward=ward_list[1])
-# print(dict(zip(year_list, stat_list)))
+# ward_price_list, ward_list = value_per_ward(resi_house_data, 'Average', year='All')
+# print(ward_price_list, ward_list)
